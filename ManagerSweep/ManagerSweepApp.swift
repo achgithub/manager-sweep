6b3869e25e09140f89c5c7579249b9f338d3ff0b
Andrew Harris <1.andy.c.harris@gmail.com>
2026-05-22 08:45:16 +0100

    Initial Commit

---
+++ ManagerSweep/ManagerSweepApp.swift
@@ -0,0 +1,32 @@
+//
+//  ManagerSweepApp.swift
+//  ManagerSweep
+//
+//  Created by Andrew Harris on 22/05/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ManagerSweepApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
